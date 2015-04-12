b23c1063f45dd8208e583c52c3d27e7b13e8c485
jordanmather <jordanjmather@gmail.com>
2015-04-12 14:40:58 -0500

    replaced code with code from class

--- ResponseThread.java
+++ ResponseThread.java
@@ -1,69 +1,59 @@
-import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 public class ResponseThread extends Thread
 {
 	ConnectedClient theClient;
 	LinkedList<ConnectedClient> allTheClients;
-	private int fileLength = 0;
-	private String fileName = "";
-	int bytesRead = 0;
-	int current = 0;
-
+	private static String theFileName = "";
+	private static int theSize = -1;
+	
 	public ResponseThread(ConnectedClient theClient)
 	{
 		this.theClient = theClient;
 		this.allTheClients = Driver.theClients;
 	}
-
+	
 	public void run()
 	{
-		this.theClient.sendMessage("Do you want to share or download?");
-		String theAnswer = this.theClient.getMessage();
-		this.theClient.sendMessage(theAnswer);
-
-		//share
-		if(theAnswer.equalsIgnoreCase("share"))
+		//is the client sharing or getting?
+		String clientMode = this.theClient.getMessage();
+		
+		if(clientMode.equals("share"))
 		{
-			//get the length of the file
-			//I tried reading from the client, but it would send the picture instead
-			//fileLength = Integer.parseInt(this.theClient.getMessage());
-			fileLength = Integer.parseInt("60000"); //hard-coded to the size of the picture used for now
+			//Wait for the client to tell us the name of the file he is about to send
+			ResponseThread.theFileName = this.theClient.getMessage();
+			ResponseThread.theSize = Integer.parseInt(this.theClient.getMessage());
+			this.theClient.initBytes(theSize, true);
+			System.out.println("Sharing Mode");
 			
-			//get the name of the file
-			//again, tried to read from the client, but no luck, hard-coded for now
-			fileName = /*this.theClient.getMessage();*/ "downloaded-file.jpg";
-
-			//receive the file
-			try {
-				byte[] theByteArray = new byte[fileLength];
-				InputStream is = theClient.getTheSocket().getInputStream();
-				FileOutputStream fos = new FileOutputStream("src\\myFiles\\" + fileName);
-				BufferedOutputStream bos = new BufferedOutputStream(fos);
-				bytesRead = is.read(theByteArray, 0, theByteArray.length);
-				current = bytesRead;
-				
-				do
+			//we need to request bytes from our connectClient that
+			//other connected clients need
+			//*****Write Code HERE****
+		}
+		else
+		{
+			//do get stuff
+			System.out.println("Getting Mode");
+			
+			//send the client the name of the file
+			this.theClient.sendMessage(ResponseThread.theFileName);
+			this.theClient.sendMessage("" + ResponseThread.theSize);
+			this.theClient.initBytes(theSize, false);
+			
+			//wait for client requests for receiving a byte
+			String whichByte = this.theClient.getMessage();
+			for(ConnectedClient cc : Driver.theNotBusyClients)
+			{
+				if(cc != this.theClient && cc.hasByte(Integer.parseInt(whichByte)))
 				{
-					bytesRead = is.read(theByteArray, current, (theByteArray.length-current));
-					if(bytesRead >= 0)
-					{
-						current += bytesRead;
-					}
-				}while(bytesRead > -1);
-				
-				bos.write(theByteArray, 0, theByteArray.length);
-				bos.flush();
-				System.out.println("File " + fileName + " downloaded (" + current + " bytes read)");
-				if (fos != null) fos.close();
-			    if (bos != null) bos.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+					//share the byte with this guy
+				}
 			}
 			
 		}
